2a9d9ca8e382c59dba02d11cb2321d6963fe3e6f
itadmin <itadmin@ravi.lapr218.successivetech.com>
2020-12-17 19:53:47 +0530

    feat:Getting started with React (Component and Tools)

--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-
 import { TextField } from '../../components';
 
-import { Div } from '../../components/TextField/style';
-
 const TextFieldDemo = () => (
-  <Div>
+  <div>
     <p><b>This is a Disabled Input</b></p>
     <TextField disabled="This is a disable" value="disabled input" />
     <p><b> A Valid Input</b></p>
     <TextField value="accessible" />
     <p><b>An input with Errors </b></p>
     <TextField error="Could not be more than" value="101" />
-  </Div>
+  </div>
 );
 
 export default TextFieldDemo;
